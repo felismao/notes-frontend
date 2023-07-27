0459917aed42370e6dcf19417f47b690a343e025
felismao <81837957+felismao@users.noreply.github.com>
2023-07-27 18:30:59 +1000

    Some layouting, some issues with child of MainLayoutComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
 const routes: Routes = [
-  {path:'', component: MainLayoutComponent, children:[
+  {path:'', component: MainLayoutComponent, children: [
     {path: ' ',component: NotesListComponent}
   ]}
 ];
