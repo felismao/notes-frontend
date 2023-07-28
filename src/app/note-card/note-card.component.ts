275bcaa56ab7b5705a833ac66832eef30f27c054
felismao <81837957+felismao@users.noreply.github.com>
2023-07-28 20:31:36 +1000

    Improve UI

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteCardComponent implements OnInit {
 
+  @ViewChild('truncator', {static: true}) truncator!: ElementRef<HTMLElement>;
+  @ViewChild('bodyText', {static: true}) bodyText!: ElementRef<HTMLElement>;
   constructor() { }
 
   ngOnInit(): void {
+    //check for text overflow
+    let style = window.getComputedStyle(this.bodyText.nativeElement,null);
+    let viewableHeight = parseInt(style.getPropertyValue("height"),10);
+
   }
 
 }
