ea4fb84c7ae69af720c66f0f75e490462abc4624
felismao <81837957+felismao@users.noreply.github.com>
2023-08-03 17:59:40 +1000

    Front end design finished

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, Input } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -7,15 +7,26 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class NoteCardComponent implements OnInit {
 
+  @Input() title!: string;
+  @Input() body!: string;
+
   @ViewChild('truncator', {static: true}) truncator!: ElementRef<HTMLElement>;
   @ViewChild('bodyText', {static: true}) bodyText!: ElementRef<HTMLElement>;
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
     //check for text overflow
     let style = window.getComputedStyle(this.bodyText.nativeElement,null);
     let viewableHeight = parseInt(style.getPropertyValue("height"),10);
 
+    if (this.bodyText.nativeElement.scrollHeight >  viewableHeight){
+        // if there is no text overflow , show fade out
+        this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    }
+    else{
+      // else hide fade out
+      this.renderer.setStyle(this.truncator.nativeElement,'display', 'none');
+    }
   }
 
 }
