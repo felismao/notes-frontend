4965d5b0d02f9cb3c51b10d3af3ab90828218071
felismao <81837957+felismao@users.noreply.github.com>
2023-08-07 20:01:11 +1000

    animation finsih

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,4 +1,4 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
@@ -35,7 +35,47 @@ import { NotesService } from 'src/app/shared/notes.service';
         })),
         // animate final state
         animate(200)
+      ]),
+      transition('* => void', [
+        //scale up
+        animate(50, style({
+          transform: 'scale(1.05)'
+        })),
+        // scale down to normal + start fade out
+        animate(50, style({
+          transform:'sale(1)',
+          opacity: 0.75
+        })),
+        //scale down + fade out complete
+        animate('120ms ease-out', style({
+          opacity: 0,
+          transform:'scale(0.68)'
+        })),
+        // animate spacing
+        animate('150ms ease-out',style({
+          height : 0,
+          'margin-bottom': 0,
+          paddingTop:0,
+          paddingBottom:0,
+          paddingRight:0,
+          paddingLeft:0
+        }))
       ])
+    ]),
+    trigger('listAnim',[
+      transition('*=>*', [
+        query(':enter', [
+          style({
+            opacity:0,
+            height:0,
+          }),
+          stagger(100, [
+            animate('0.2s ease')
+          ])
+        ], {
+          optional:true
+        })
+      ] )
     ])
   ]
 })
