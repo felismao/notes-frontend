e4690dc2e971e6976537266e5128fc9b66dc9c83
felismao <81837957+felismao@users.noreply.github.com>
2023-08-07 19:47:08 +1000

    entry animation added

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
@@ -5,7 +6,38 @@ import { NotesService } from 'src/app/shared/notes.service';
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
+  animations:[
+    trigger('itemAnim', [
+      //Entry animation
+      transition('void => *', [
+        //set state
+        style({
+          height : 0,
+          opacity :0,
+          transform: 'scale(0.85)',
+          'margin-bottom': 0,
+
+          //need to expand the padding properties
+          paddingTop:0,
+          paddingBottom:0,
+          paddingRight:0,
+          paddingLeft:0
+        }),
+        // animate spacing
+        animate('50ms',style({
+          height: '*',
+          'margin-bottom' : '*',
+          paddingTop:'*',
+          paddingBottom:'*',
+          paddingRight:'*',
+          paddingLeft:'*',
+        })),
+        // animate final state
+        animate(200)
+      ])
+    ])
+  ]
 })
 export class NotesListComponent implements OnInit {
 
